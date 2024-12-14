baa1ded3c039ffed3375facdebc6b8617d99c9ac
hasna1810 <121877642+hasna1810@users.noreply.github.com>
2024-12-14 20:04:36 +0530

    Update App.js

--- frontend/App.js
+++ frontend/App.js
@@ -1,1 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';  // Import Navbar component
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import AdminDashboard from './components/AdminDashboard'; // Admin Dashboard
+import Login from './components/Login'; // Login page for users
 
+const App = () => {
+  return (
+    <Router>
+      <Navbar /> {/* Add Navbar component here */}
+      <Routes>
+        {/* User Routes */}
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+
+        {/* Admin Route */}
+        <Route path="/admin/dashboard" element={<AdminDashboard />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
